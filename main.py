53b91fd8deddadcb3d8bfb84bc90196ce0b65133
Yatin Sharma <yatinsharma811@gmail.com>
2024-04-03 20:18:44 +0530

    model trainer added

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from cnnClassifier.pipeline.stage_02_prepare_base_model import (
     PrepareBaseModelTrainingPipeline,
 )
+from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
-
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = DataIngestionTrainingPipeline()
@@ -15,12 +16,24 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-STAGE_NAME = "Prepare Base Model"
 
+STAGE_NAME = "Prepare Base Model"
 try:
     logger.info(f"************")
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise (e)
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"************")
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
 
 project_name = "cnnClassifier"
 
@@ -22,7 +22,7 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
+    "templates/index.html",
 ]
 
 for filepath in list_of_files:
@@ -32,11 +32,11 @@ for filepath in list_of_files:
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
-    
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty files: {filepath}")
-        
+
     else:
         logging.info(f"{filename} already exists")
